dda0e3c0ac52c67ef15fe57ec29ca50565bd2975
mithun <mithunreddythunga@gmail.com>
2017-08-04 03:29:02 -0500

    scrollReveal effect added

--- js/main.js
+++ js/main.js
@@ -4,7 +4,36 @@ $(document).ready(function(){
         var linkHref = $(this).attr('href');
         $('html,body').animate({
             scrollTop:$(linkHref).offset().top
-        });
+        },1000);
         e.preventDefault();
     });
+});
+//this script for scrollReveal
+
+window.sr = ScrollReveal();
+sr.reveal('.navbar-header', {
+    duration: 1000,
+    origin:'top'
+});
+
+sr.reveal('.left', {
+    duration: 1000,
+    origin: 'left'
+});
+sr.reveal('.right',{
+    duration:1000,
+    delay: 1000,
+    origin: 'right'
+});
+sr.reveal('#service-body', {
+    duration: 1000,
+    origin: 'top'
+});
+sr.reveal('.port', {
+    duration: 1000,
+    origin: 'top'
+});
+sr.reveal('.contact', {
+    duration: 1000,
+    origin: 'top'
 });
